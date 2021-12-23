54c175ab1cbf3c9fbd5935604bff5505734a5838
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-12-23 21:00:53 +0100

    Update cracker.py

--- XOR/cracker.py
+++ XOR/cracker.py
@@ -8,13 +8,14 @@ file_to_crypt = input ("\nEnter the name of the file to crack :")
 output = input ("\nEnter the name of output file :")
 def take_number():
     global number_input
-    number_input = input("\nEnter key lenght : ")
+    number_input = input("\nEnter key lenght (leave empty for random lenght): ")
     if len(number_input) == 0:
-        # def randStar(chars = string.ascii_uppercase + string.digits, N=number):
-        #     return ''.join(random.choice(chars) for _ in range(N))
-        # word_key = randStar(chars='1234567890')
-        print("lol")
+        global num_a
+        global num_b
+        num_a = int(input ("\nSelect start size lenght :"))
+        num_b = int(input ("\nSelect final size lenght :"))
     else:
+        global number
         number = int(number_input)
 take_number()
 def take_letter():
@@ -33,34 +34,41 @@ take_letter()
 cwd = os.getcwd()
 output_path = cwd + "\\" + output
 z = 0
+list = []
 for z in range(1, 100000000) :
     i = 0
     if len(number_input) == 0:
-        number = random.randint(1, 15)
+        number = random.randint(num_a, num_b)
     def randStr(chars = string.ascii_uppercase + string.digits, N=number):
         return ''.join(random.choice(chars) for _ in range(N))
     word_key = randStr(chars=letter)
-    pre_keys = sha256(word_key.encode('utf-8')).digest()
-    hash_keys = hashlib.sha512(pre_keys)
-    hash_digest = hash_keys.hexdigest()
-    keys = sha256(hash_digest.encode('utf-8')).digest()
-    with open (file_to_crypt, 'rb') as f_file_to_crypt:
-        text_block = f_file_to_crypt.read()
-        text_block_2 = bytes()
-    for i in range (len(text_block)):
-        c = text_block[i]
-        j = i % len(keys)
-        b = bytes ([c^keys[j]])
-        text_block_2 = text_block_2 + b
-    la_phrase = f'For key : {word_key} ; done : {text_block_2}'
-    command = f'cmd /c "echo {la_phrase} >> {output_path} "'
-    try:
-        chad = text_block_2.decode('ascii')
-        print ("\n" + chad) 
-        print(f"\nfor key {word_key} LIGMA {z}")
-        input ("\nKey Cracked!!!(press enter to exit)")
-        os.system(command)
-        break
-    except:
-        print(f"for key {word_key} LIGMA {z}")
+    list.append(word_key)
+    if word_key in list:
+        pre_keys = sha256(word_key.encode('utf-8')).digest()
+        hash_keys = hashlib.sha512(pre_keys)
+        hash_digest = hash_keys.hexdigest()
+        keys = sha256(hash_digest.encode('utf-8')).digest()
+        with open (file_to_crypt, 'rb') as f_file_to_crypt:
+            text_block = f_file_to_crypt.read()
+            text_block_2 = bytes()
+        for i in range (len(text_block)):
+            c = text_block[i]
+            j = i % len(keys)
+            b = bytes ([c^keys[j]])
+            text_block_2 = text_block_2 + b
+        la_phrase = f'For key : {word_key} ; done : {text_block_2}'
+        command = f'cmd /c "echo {la_phrase} >> {output_path} "'
+        try:
+            chad = text_block_2.decode('ascii')
+            print ("\n" + chad) 
+            print(f"\nfor key {word_key} LIGMA {z}")
+            blablabla = input ("\nKey Cracked!!!(press enter to exit or type continue to follow cracking)")
+            if blablabla == "continue":
+                print("CONTINUING!!!")
+            else:
+                os.system(command)
+                print (list)
+                break
+        except:
+            print(f"for key {word_key} LIGMA {z}")
     z = z + 1 
